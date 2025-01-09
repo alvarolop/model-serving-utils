e5644cc4d2a9735337cd519893578df0dc2739de
Alvaro Lopez Medina <alvarolop.medina@gmail.com>
2025-01-09 18:08:07 +0100

    Improvements to model serving. Reducing drastically permissions required and optimizing upload

--- gitops/model/run.sh
+++ gitops/model/run.sh
@@ -2,7 +2,7 @@
 ARGOCD_APP_NAME=vllm-mistral-7b
 
 # Load environment variables
-DATA_SCIENCE_PROJECT_NAMESPACE="vllm-mistral-7b-instruct-v0.2"
+DATA_SCIENCE_PROJECT_NAMESPACE="vllm-mistral-7b"
 
 helm template . --name-template ${ARGOCD_APP_NAME} \
   --set instanceName="vllm-mistral-7b" \
